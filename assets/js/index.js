70682ab7d063bd7524a30599e062d7cf6db4048a
Adrián Serrano <adrianno_3@hotmail.com>
2022-01-10 10:07:49 +0100

    pagination and controllers

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,3 @@
-var clients = [
-    { "Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false },
-    { "Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true },
-    { "Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false },
-    { "Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true },
-    { "Name": "Ramona Benton", "Age": 32, "Country": 3, "Address": "Ap #614-689 Vehicula Street", "Married": false }
-];
-
 fetch("../resources/employees.json")
 .then(res => res.json())
 .then(data => jsGridFun(data))
@@ -15,10 +7,57 @@ function jsGridFun(data){
         width: "100%",
         //height: "400px",
     
+        autoload: false,
+        controller: {
+            loadData: function(filter) {
+                return $.ajax({
+                    type: "GET",
+                    url: "../resources/employees.json",
+                    data: filter
+                });
+            },
+            
+            insertItem: function(item) {
+                return $.ajax({
+                    type: "POST",
+                    url: "../resources/employees.json",
+                    data: item
+                });
+            },
+            
+            updateItem: function(item) {
+                return $.ajax({
+                    type: "PUT",
+                    url: "../resources/employees.json",
+                    data: item
+                });
+            },
+            
+            deleteItem: function(item) {
+                return $.ajax({
+                    type: "DELETE",
+                    url: "../resources/employees.json",
+                    data: item
+                });
+            },
+        },
+
         inserting: true,
         editing: true,
         sorting: true,
         paging: true,
+
+        pagerContainer: null,
+        pageIndex: 1,
+        pageSize: 3,
+        pageButtonCount: 5,
+        pagerFormat: "Pages: {first} {prev} {pages} {next} {last}    {pageIndex} of {pageCount}",
+        pagePrevText: "Prev",
+        pageNextText: "Next",
+        pageFirstText: "First",
+        pageLastText: "Last",
+        pageNavigatorNextText: "...",
+        pageNavigatorPrevText: "...",
     
         data: data,
     
